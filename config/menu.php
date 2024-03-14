00cdc470da3beb81d218179d75f7a354980e635d
elradinn <johnalfredalfonso.dev@gmail.com>
2024-03-15 03:41:21 +0800

    menu improvements

--- config/menu.php
+++ config/menu.php
@@ -3,10 +3,10 @@
 return [
     [ 'title' => 'Browse Dish', 'path' => 'browseDishs', 'roles' => 'USER', 'show' => true ],
     [ 'title' => 'Cart Detail', 'path' => 'cartDetails', 'roles' => 'USER', 'show' => false ],
+    [ 'title' => 'Orders', 'path' => 'orderHeaders', 'roles' => 'ADMIN', 'show' => true ],
+    [ 'title' => 'Dishes', 'path' => 'dishs', 'roles' => 'ADMIN', 'show' => true ],
     [ 'title' => 'Cart', 'path' => 'cartHeaders', 'roles' => 'USER', 'show' => true ],
-    [ 'title' => 'Category', 'path' => 'categories', 'roles' => 'ADMIN', 'show' => true ],
-    [ 'title' => 'Dish', 'path' => 'dishs', 'roles' => 'ADMIN', 'show' => true ],
+    [ 'title' => 'Categories', 'path' => 'categories', 'roles' => 'ADMIN', 'show' => true ],
     [ 'title' => 'Order Detail', 'path' => 'orderDetails', 'roles' => 'ADMIN', 'show' => false ],
-    [ 'title' => 'Order', 'path' => 'orderHeaders', 'roles' => 'ADMIN', 'show' => true ],
-    [ 'title' => 'User Account', 'path' => 'userAccounts', 'roles' => 'ADMIN', 'show' => true ]
+    [ 'title' => 'User Accounts', 'path' => 'userAccounts', 'roles' => 'ADMIN', 'show' => true ]
 ];
