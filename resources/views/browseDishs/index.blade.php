0dcd023d661188cfcb0b08230d29e802bab2c803
elradinn <johnalfredalfonso.dev@gmail.com>
2024-03-16 18:41:11 +0800

    cart fix

--- resources/views/browseDishs/index.blade.php
+++ resources/views/browseDishs/index.blade.php
@@ -7,28 +7,6 @@
       <p>Check Our <span>Menu</span></p>
     </div>
 
-    <ul class="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
-      <li class="nav-item">
-        <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#menu-starters">
-          <h4>Silog</h4>
-        </a>
-      </li>
-      <!-- End tab nav item -->
-
-      <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-breakfast">
-          <h4>Beverages</h4>
-        </a><!-- End tab nav item -->
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-lunch">
-          <h4>Lunch</h4>
-        </a>
-      </li>
-      <!-- End tab nav item -->
-    </ul>
-
     <!-- Tab Contents -->
     <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
       <!-- Tab Pane -->
@@ -41,7 +19,6 @@
         <div class="row gy-5">
           @foreach ($browseDishs as $browseDish)
           <div class="col-lg-4 menu-item">
-            @if ($browseDish->category_name == 'Silog')
             <a href="/storage/dishs/{{$browseDish->image}}" class="glightbox"><img
                 src="/storage/dishs/{{$browseDish->image}}" class="menu-img img-fluid" alt="" /></a>
             <h4>{{$browseDish->name}}</h4>
@@ -56,7 +33,6 @@
               <button class="add-order" type="submit" onclick="alert('Dish added successfully')">Add to Cart</button>
             </form>
           </div>
-          @endif
           @endforeach
         </div>
       </div>
@@ -71,8 +47,8 @@
 
         <div class="row gy-5">
           @foreach ($browseDishs as $browseDish)
+          @if ($browseDish->category_name == 'Drinks')
           <div class="col-lg-4 menu-item">
-            @if ($browseDish->category_name == 'Drinks')
             <a href="/storage/dishs/{{$browseDish->image}}" class="glightbox"><img
                 src="/storage/dishs/{{$browseDish->image}}" class="menu-img img-fluid" alt="" /></a>
             <h4>{{$browseDish->name}}</h4>
